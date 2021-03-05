c9719409d395d6b431044bddfd057f9cb8f38dd5
el <elrophilskwaila@gmail.com>
2021-03-05 12:46:02 +0300

    Added jquery toggle effect and styled my form

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('button#btn-man').hide();
+    $('select#size-name')
+})
+
+$(document).ready(function(){
+    event.preventDefault();
+    $('.navbar-toggler').slideToggle("slow");
+})
+
+$(document).ready(function(){
+    $('.navbar-toggler').click(function(){
+        $('.form-slide').slideToggle("fast")
+    });
+})
