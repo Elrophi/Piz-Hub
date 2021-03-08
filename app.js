13469ed66db2f88b201eaa8f82e02b9cbf090450
el <elrophilskwaila@gmail.com>
2021-03-08 04:02:42 +0300

    Added delivery, and order times

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ $(document).ready(function(){
     });
 })
 
-
 $(document).ready(function(){
     $('.custom-select').click(function(){
         $('.btn').show("slow")
@@ -38,20 +37,31 @@ function myForm(){
     var mySize = document.getElementById('size-name').value;
     var myCrust = document.getElementById('crust-name').value;
     var myTop = document.getElementById('top-name').value;
+    var myCheck = document.getElementById('ch1');
 
-    let piz = new Pizza(mySize, myCrust, myTop);
-    if(mySize == "No"){
-        alert('please select a size')
+    let piz = new Pizza(mySize, myCrust, myTop, myCheck);
+    if(myTop == "No" || mySize == "No"){
+        alert('please select a size and flavour')
     }else{
     // document.getElementById('popForm').style.display = "block"; 
     var res = prompt("How many would you like to order");
-    document.getElementById('theNo').innerHTML = res;
-    document.getElementById('theSize').innerHTML = mySize;
-    document.getElementById('theCrust').innerHTML = myCrust;
-    document.getElementById('theTop').innerHTML = myTop;
-    document.getElementById('popForm').style.display = "block"; 
-
+    var add = prompt("For delivery to your home enter your address (fee = 250kshs)! if not just type NO")
+    if(res == 0){
+        $('.form-slide').show()
+        document.getElementById('size-name').value="No";
+        document.getElementById('crust-name').value="No";
+        document.getElementById('top-name').value="No";
+        document.getElementById('ch1').value = "";
+    }else{
+        document.getElementById('theDeliv').innerHTML = add;
+        document.getElementById('theNo').innerHTML = res;
+        document.getElementById('theSize').innerHTML = mySize;
+        document.getElementById('theCrust').innerHTML = myCrust;
+        document.getElementById('theTop').innerHTML = myTop;
+        document.getElementById('popForm').style.display = "block"; 
     }
+    }
+    
 }
 
 function exitForm(){
@@ -61,5 +71,15 @@ function exitForm(){
     document.getElementById('size-name').value="0";
     document.getElementById('crust-name').value="0";
     document.getElementById('top-name').value="0";
+    document.getElementById('ch1').value = "";
+
 
 }
+
+
+
+// var markedCheckbox = document.getElementsById('ch1').value;  
+//         for (var checkbox of markedCheckbox) {  
+//           if (checkbox.checked)  
+//             document.getElementById('theTops').value = checkbox;  
+//       }  
